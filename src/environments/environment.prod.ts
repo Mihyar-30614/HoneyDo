10fb728ee8d7c00f23f89602d49924d57a55334f
Mihyar Al Masalma <eng.mihyear@gmail.com>
2025-05-13 01:53:04 -0300

    Adding firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
 	production: true,
 	firebaseConfig: {
-		apiKey: "AIzaSyDiryIyRULcuI4hspcckFegnkR8FkMrmfk",
-		authDomain: "honeydos-84e3d.firebaseapp.com",
-		projectId: "honeydos-84e3d",
-		storageBucket: "honeydos-84e3d.firebasestorage.app",
-		messagingSenderId: "125150172805",
-		appId: "1:125150172805:web:0779a9302787bc6afa7bec",
-		measurementId: "G-WN0G2GFXT3"
+		apiKey: "AIzaSyCQ3Bb7BSwUl0gZ9UN_3-25PkboHUGziUE",
+		authDomain: "honeydolist-9c5ce.firebaseapp.com",
+		projectId: "honeydolist-9c5ce",
+		storageBucket: "honeydolist-9c5ce.firebasestorage.app",
+		messagingSenderId: "244841171356",
+		appId: "1:244841171356:web:6ac145d2d491420ad66059",
+		measurementId: "G-SCEDQ8D4ND"
 	}
 };
