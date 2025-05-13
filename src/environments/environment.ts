46af461652d7f8a7aec76132ada6f5ab062570a4
Mihyar Al Masalma <eng.mihyear@gmail.com>
2025-05-13 02:01:13 -0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-	production: false,
-	firebaseConfig: {
-		apiKey: "AIzaSyCQ3Bb7BSwUl0gZ9UN_3-25PkboHUGziUE",
-		authDomain: "honeydolist-9c5ce.firebaseapp.com",
-		projectId: "honeydolist-9c5ce",
-		storageBucket: "honeydolist-9c5ce.firebasestorage.app",
-		messagingSenderId: "244841171356",
-		appId: "1:244841171356:web:6ac145d2d491420ad66059",
-		measurementId: "G-SCEDQ8D4ND"
-	}
+	production: false
 };
 
 /*
---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyCQ3Bb7BSwUl0gZ9UN_3-25PkboHUGziUE",
+	authDomain: "honeydolist-9c5ce.firebaseapp.com",
+	projectId: "honeydolist-9c5ce",
+	storageBucket: "honeydolist-9c5ce.firebasestorage.app",
+	messagingSenderId: "244841171356",
+	appId: "1:244841171356:web:6ac145d2d491420ad66059",
+	measurementId: "G-SCEDQ8D4ND"
+};
+
+;
